db459b9b84730d571116c0223241fd134fbeea36
Asım <asimzorlu@gmail.com>
2019-10-24 16:04:40 +0300

    Add files via upload
    some bug fixes

--- follow.py
+++ follow.py
@@ -2,27 +2,34 @@ import pyautogui as pg
 import time
 
 def open_wo_login():
-    pg.click(333, 752)
+    pg.click(334, 1058)
     time.sleep(2)
 
 def refresh():
-    pg.click(86, 49)  # refresh
+    pg.click(127, 74)  # refresh
     time.sleep(8)
 
 def follower_list():
     for m in range(6):
         pg.press('tab')
-        time.sleep(2)
+        time.sleep(0.5)
     pg.press('enter')
     time.sleep(5)
 
 def follow_all():
     for j in range(0, 100, 4):
-        for k in range(5+j):
-            pg.press('tab')
-            time.sleep(2)
-        pg.press('enter')
-        time.sleep(5)
+        if j == 32:
+            for m in range(71):
+                pg.press('tab')
+                time.sleep(0.25)
+            pg.press('enter')
+            time.sleep(5)
+        else:
+            for k in range(5+j):
+                pg.press('tab')
+                time.sleep(0.25)
+            pg.press('enter')
+            time.sleep(5)
 
 def main():
     open_wo_login()
